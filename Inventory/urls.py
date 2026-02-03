0d4afd232b62e8df717c63b8a64d3f897fabd2c4
Bala-16 <ayyappanbala1611@gmail.com>
2026-02-03 15:15:34 +0530

    AP

---
+++ Inventory/urls.py
@@ -0,0 +1,54 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.nav, name='home'),   # ← THIS WAS MISSING
+    path('nav/', views.nav, name='nav'),
+    path('jobcard/customer_report/', views.customer_report, name='customer_report'),
+    path('jobcard/quotation/', views.quotation, name='quotation'),
+    path('jobcard/designer_report/', views.designer_report, name='designer_report'),
+    path('process_report/', views.process_report, name='process_report'),
+    path('quotation_customer/', views.quotation_customer, name='quotation_customer'),
+    path('jobcard/job_creation/', views.job_creation, name='job_creation'),
+    path('jobcard/job_list/', views.job_list, name='job_list'),
+    path('mastercreation/company_creation/', views.company_creation, name='company_creation'),
+    path('mastercreation/party_creation/', views.party_creation, name='party_creation'),
+    path('mastercreation/user_type_creation/', views.user_type_creation, name='user_type_creation'),
+    path('fusing/', views.fusing, name='fusing'), 
+    path('printing/', views.printing, name='printing'),
+    path('consumable/', views.consumable, name='consumable'),
+    path('accounts/', views.accounts, name='accounts'),
+    path('delivery/', views.delivery, name='delivery'),
+    path('dashboard/', views.nav, name='dashboard'),
+    path('quotationform/', views.quotationform, name='quotationform'),
+    path('customer_report/', views.customer_report, name='customer_report'),
+
+    path('mastercreation/operator/', views.operator, name='operator'),
+    path('mastercreation/measurement/', views.measurement, name='measurement'),
+    path('mastercreation/userpermission/', views.userpermission, name='userpermission'),
+    path('mastercreation/userscreen/', views.userscreen, name='userscreen'),
+    path('mastercreation/machinecreation/', views.machinecreation, name='machinecreation'),
+    path('mastercreation/shift/', views.shift, name='shift'),
+    path('mastercreation/paymentterms/', views.paymentterms, name='paymentterms'),
+    path('mastercreation/consumablecreation/', views.consumablecreation, name='consumablecreation'),
+    path('mastercreation/fabrictype/', views.fabrictype, name='fabrictype'),
+    path('mastercreation/rollsize/', views.rollsize, name='rollsize'),
+    path('mastercreation/gapcreation/', views.gapcreation, name='gapcreation'),
+ path('mastercreation/jobtypecreation/', views.jobtypecreation, name='jobtypecreation'),
+ path('mastercreation/customerstatus/', views.customerstatus, name='customerstatus'),
+
+    path('fusing/job-no-wise-report/', views.job_no_wise_report, name='job_no_wise_report'),
+    path('fusing/fusing-operator-wise-report/', views.fusing_operator_wise_report, name='fusing_operator_wise_report'),
+    path('fusing/fusing-machine-wise-report/', views.fusing_machine_wise_report, name='fusing_machine_wise_report'),
+    path('fusing/date-wise-report/', views.date_wise_report, name='date_wise_report'),
+    path('fusing/fusing-customer-wise-report/', views.fusing_customer_wise_report, name='fusing_customer_wise_report'),
+    path('fusing/fusing-production-entry/', views.fusing_production_entry, name='fusing_production_entry'),
+
+
+    path('printing/printer_customer/',views.printer_customer, name='printer_customer'),
+     path('printing/printer_data/',views.printer_data, name='printer_data'),
+      # path('printing/printer_jobcard/',views.printer_jobcard, name='printer_jobcard'),
+       path('printing/printer_machine/',views.printer_machine, name='printer_machine'),
+        path('printing/printer_operator/',views.printer_operator, name='printer_operator'),
+         path('printing/printer_production/',views.printer_production, name='printer_production'),
+]
---
+++ Inventory/views.py
@@ -0,0 +1,97 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def nav(request):
+    return render(request,'nav.html')
+
+def customer_report(request):
+    return render(request,'jobcard/customer_report.html')
+def quotation(request):
+    return render(request,'jobcard/quotation.html',)
+def designer_report(request):
+    return render(request,'jobcard/designer_report.html',)
+def process_report(request):
+    return render(request,'jobcard/process_report.html',)
+def quotation_customer(request):
+    return render(request,'jobcard/quotation_customer.html',)
+def job_creation(request):
+    return render(request,'jobcard/job_creation.html',)
+def job_list(request):
+    return render(request,'jobcard/job_list.html',)
+def company_creation(request):
+    return render(request,'mastercreation/company_creation.html',)
+def party_creation(request):
+    return render(request,'mastercreation/party_creation.html',)
+def user_type_creation(request):
+    return render(request,'mastercreation/user_type_creation.html',)
+def fusing(request):
+    return render(request,'fusing/fusing.html',)
+def printing(request):
+    return render(request,'printing/printing.html',)
+def consumable(request):
+    return render(request,'consumable/consumable.html',)
+def accounts(request):
+    return render(request,'accounts/accounts.html',)
+def  delivery(request):
+    return render(request,'delivery/delivery.html',)
+def dashboard(request):
+    return render(request,'dashboard/dashboard.html',)
+def quotationform(request):
+    return render(request,'jobcard/quotationform.html',)
+def operator(request):
+    return render(request,'mastercreation/operator.html',)
+def measurement(request):
+    return render(request,'mastercreation/measurement.html',)
+def userpermission(request):
+    return render(request,'mastercreation/userpermission.html',)
+def userscreen(request):
+    return render(request,'mastercreation/userscreen.html',)
+def machinecreation(request):
+    return render(request,'mastercreation/machinecreation.html',)
+def shift(request):
+    return render(request,'mastercreation/shift.html',)
+def paymentterms(request):
+    return render(request,'mastercreation/paymentterms.html',)
+def consumablecreation(request):
+    return render(request,'mastercreation/consumablecreation.html',)
+def fabrictype(request):
+    return render(request,'mastercreation/fabrictype.html',)
+def rollsize(request):
+    return render(request,'mastercreation/rollsize.html',)
+def gapcreation(request):
+    return render(request,'mastercreation/gapcreation.html',)
+
+def jobtypecreation(request):
+    return render(request,'mastercreation/jobtypecreation.html',)
+def customerstatus(request):
+    return render(request,'mastercreation/customerstatus.html',)
+
+
+def job_no_wise_report(request):
+    return render(request,'fusing/job-no-wise-report.html',)
+def fusing_operator_wise_report(request):
+    return render(request,'fusing/fusing-operator-wise-report.html',)
+def fusing_machine_wise_report(request):
+    return render(request,'fusing/fusing-machine-wise-report.html',)
+def date_wise_report(request):
+    return render(request,'fusing/date-wise-report.html',)
+def fusing_customer_wise_report(request):
+    return render(request,'fusing/fusing-customer-wise-report.html',)
+def fusing_production_entry(request):
+    return render(request,'fusing/fusing-production-entry.html',)
+
+def printer_customer(request):
+    return render(request,'printing/printer_customer.html',)
+def printer_data(request):
+    return render(request,'printing/printer_data.html',)
+# def pinter_jobcard(request):
+#     return render(request,'printing/printer_jobcard.html',)
+def printer_machine(request):
+    return render(request,'printing/printer_machine.html',)
+def printer_operator(request):
+    return render(request,'printing/printer_operator.html',)
+def printer_production(request):
+    return render(request,'printing/printer_production.html',)
+
+
